c3ffd7fadc74272223636a5c0ef94d669b757220
Tomáš Krže <106106200+TomasKsk@users.noreply.github.com>
2023-12-03 23:05:53 +0100

    Update +page.svelte
    adding global variables and making functions outside the onMount lifecycle function for better readability

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,44 +2,55 @@
     import { onMount, onDestroy } from 'svelte';
     import { writable } from 'svelte/store';
 
-    // handle clicks here
+    const operandArr = ["÷", "x", "-", "+"];
+    const dataTypes = ['header', 'number', 'comment', 'operator'];
+
+    let calcMem = writable([]);
+    let calcDisp = writable('');
+    let delKey = writable('C');
+
+
+    // handle ALL clicks here
+    const handleClick = (e) => {
+        const num = e.target.innerHTML;
+        console.log(num);
+
+        if (!isNaN(+num) && typeof +num === 'number') {
+            handleNumberClick(num, e);
+        }
+    }
+
+    const handleNumberClick = (num, e) => {
+        if (e.target.matches('button')) {
+            // console.log('it is a num button')
+            if (!$calcMem.includes('=')) {
+                // concat number to the existing text variable
+                calcDisp.update((prevDisp) => prevDisp + num);
+
+            }
+        }
+    }
     // lifecycle function for when the component is firstly mounted
     onMount(() => {
-        const handleClick = (e) => {
-            const num = e.target.innerHTML;
-            console.log(num);
-
-            if (!isNaN(+num) && typeof +num === 'number') {
-                handleNumberClick(num, e);
-            }
-        }
-
-        const handleNumberClick = (num, e) => {
-            if (e.target.matches('button')) {
-                console.log('it is a num button')
-            }
-        }
-
         //event listeners
         const sel = document.querySelector('.container');
         sel.addEventListener('click', handleClick);
 
-        // lifecycle function to unmount the component
-        onDestroy(() => {
-            sel.removeEventListener('click', handleClick);
-        });
-
         // cleanup
-        return () => {};
+        return () => {
+            sel.removeEventListener('click', handleClick);
+        };
     });
+    
 </script>
+<slot />
 
 <div class='container'>
     <div id='calc-main' class='calc-grid'>
         <div class="calc-display">
             <div id="menu-icon-place" class="storage-window"></div>
-            <div class="calc-current"></div>
-            <div class="calc-mem"></div>
+            <div class="calc-current">{$calcDisp}</div>
+            <div class="calc-mem">{$calcMem}</div>
             <div><span class="save-button saveIcon" id="save-icon-place"></span></div>
         </div>
         <div class='calc-keys'>
@@ -58,9 +69,10 @@
             <button>3</button>
             <button>0</button>
             <button class="op5">.</button>
-            <button id="AC-key" class="op6">C</button>
+            <button id="AC-key" class="op6">{$delKey}</button>
             <button class="op7">=</button>
         </div>
+
     </div>
 
     <!-- here comes the storage -->
