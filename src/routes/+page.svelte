9c0bb2d8f67ef78a7f020296d2a6c9ec3085a000
Tomáš Krže <106106200+TomasKsk@users.noreply.github.com>
2023-12-03 20:15:08 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div id='calc-main' class='calc-grid'>
+    <div class="calc-display">
+        <div id="menu-icon-place" class="storage-window"></div>
+        <div class="calc-current"></div>
+        <div class="calc-mem"></div>
+        <div><span class="save-button saveIcon" id="save-icon-place"></span></div>
+    </div>
+    <div class='calc-keys'>
+        <button class="op1">+</button>
+        <button class="op2">-</button>
+        <button class="op3">x</button>
+        <button class="op4">÷</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>0</button>
+        <button class="op5">.</button>
+        <button id="AC-key" class="op6">C</button>
+        <button class="op7">=</button>
+    </div>
+</div>
+
